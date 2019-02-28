fd08c45b5b1c210b3eee3afcc4ef8ac9fd506281
gstacks13 <gstacks1322@gmail.com>
2019-02-28 05:56:44 -0600

    Add initial pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,26 @@
-import sys
-from os.path import abspath, dirname
-sys.path.append(dirname(dirname(abspath(__file__))))
+import configparser
+import plex_set_tracks
+import pytest
+
+# Get info from config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+PLEX_URL = config['LOGIN']['PLEX_URL']
+PLEX_TOKEN = config['LOGIN']['PLEX_TOKEN']
+
+
+@pytest.fixture(scope='session')
+def plex():
+    assert len(PLEX_URL) > 0, "Plex URL is not specified in config file."
+    assert len(PLEX_TOKEN) > 0, "Plex token is not specified in config file."
+    return plex_set_tracks.signInLocally()
+
+
+@pytest.fixture(scope='session')
+def tvshows(plex):
+    return plex.library.get("TV Shows")
+
+
+@pytest.fixture(scope='session')
+def show(tvshows):
+    return tvshows.get("Game of Thrones")
--- tests/test_plex_set_tracks.py
+++ tests/test_plex_set_tracks.py
@@ -1,15 +1,15 @@
 import plex_set_tracks
 
 
-def spoof_input(monkeypatch, inputList):
+def spoof_input(monkeypatch, input_list):
     """ Given a list of user input values, spoofs the input() function to to iterate over each item
         in the list with each successive call.
 
         Parameters:
             monkeypatch(MonkeyPatch): Monkeypatch from pytest.
-            inputList(List<str>): List of input values to iterate over.
+            input_list(List<str>): List of input values to iterate over.
     """
-    gen = (value for value in inputList)
+    gen = (value for value in input_list)
     monkeypatch.setattr('builtins.input', lambda x: next(gen))
 
 
